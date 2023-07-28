c411f394e5a42e69ab119916134e674f9b7ffe45
mxcheung <mxcheung@users.noreply.github.com>
2023-07-28 22:10:51 +1000

    Create lambda-cdk-stack.ts

---
+++ lambda-cdk/lib/lambda-cdk-stack.ts
@@ -0,0 +1,17 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as path from 'path';
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+export class LambdaCdkStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const lambdaFunction = new lambda.Function(this, 'YourLambdaFunction', {
+      runtime: lambda.Runtime.PYTHON_3_9,
+      handler: 'main.lambda_handler', // Assuming the Python file is named "main.py" and the function is named "lambda_handler"
+      code: lambda.Code.fromAsset(path.join(__dirname, '/../lambda'))
+    });
+  }
+}
