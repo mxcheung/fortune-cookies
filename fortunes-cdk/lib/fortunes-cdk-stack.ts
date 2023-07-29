1fbb171b87ef9632006d8d1f1e4118944d7e4bd0
mxcheung <mxcheung@users.noreply.github.com>
2023-07-29 15:23:18 +1000

    Update fortunes-cdk-stack.ts

--- fortunes-cdk/lib/fortunes-cdk-stack.ts
+++ fortunes-cdk/lib/fortunes-cdk-stack.ts
@@ -5,7 +5,7 @@ import * as dynamodb from 'aws-cdk-lib/aws-dynamodb'
 import * as apigateway from 'aws-cdk-lib/aws-apigateway';
 import * as path from 'path';
 
-export class LambdaCdkStack extends cdk.Stack {
+export class FortunesCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
@@ -14,12 +14,12 @@ export class LambdaCdkStack extends cdk.Stack {
     let existing_table_arn = 'arn:aws:dynamodb:us-east-1:617611017005:table/Cookies3'
  //   let table = dynamodb.Table.fromTableArn(this, "Cookies3", existing_table_arn)
 
-    let table = dynamodb.Table.fromTableName(this, "Cookies3", "Cookies3")
+    let table = dynamodb.Table.fromTableName(this, "fortunes", "fortunes")
 
 
     const lambdaFunction = new lambda.Function(this, 'YourLambdaFunction', {
       runtime: lambda.Runtime.PYTHON_3_9,
-      handler: 'main.lambda_handler', // Assuming the Python file is named "main.py" and the function is named "lambda_handler"
+      handler: 'cookies.lambda_handler', // Assuming the Python file is named "main.py" and the function is named "lambda_handler"
       code: lambda.Code.fromAsset(path.join(__dirname, '/../lambda')),
       environment: {
         DYNAMODB_TABLE: 'fortunes'
